a27e1f45dfc9c2e77bc32828b1e9348d52627ae3
kusi <appaukusi05@gmail.com>
2024-12-10 16:59:49 +0000

    updated questions

--- reinforcement/chapter-1/chapter-1.py
+++ reinforcement/chapter-1/chapter-1.py
@@ -103,3 +103,37 @@ def sum_of_pos_odd_int_below(n):
     return total
 """
 
+# R 1.8
+
+# Python allows negative integers to be used as indices into a sequence,
+# such as a string. If string s has length n, and expression s[k] is used for in-
+# dex −n ≤ k < 0, what is the equivalent index j ≥ 0 such that s[j] references
+# the same element?
+
+# my solution
+"""
+j = n + k
+"""
+
+
+# R 1.9
+
+# What parameters should be sent to the range constructor, to produce a
+# range with values 50, 60, 70, 80
+
+# my solution
+"""
+range(50, 81, 10)
+"""
+
+
+# R 1.10
+
+# What parameters should be sent to the range constructor, to produce a
+# range with values 8, 6, 4, 2, 0, −2, −4, −6, −8?
+
+# my solution
+"""
+range(8, -9, -2)
+"""
+   
